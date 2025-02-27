673dd3bce8c27df8b4f122bdfc7eb0f5e4b16ed5
lieo <lieo@gmai.com>
2025-02-27 19:11:13 +0800

    feat【网关转发】网关转发模块增加遥测，属性，事件 消息的上报

--- gatewayHandler/main.go
+++ gatewayHandler/main.go
@@ -4,7 +4,8 @@ import (
 	"flag"
 	"gatewayhandler/initialize"
 	mqttapp "gatewayhandler/mqtt"
-	"gatewayhandler/mqtt/publish"
+	"gatewayhandler/mqtt/localMqtt"
+	"gatewayhandler/mqtt/privateMqtt"
 	"gatewayhandler/register"
 	"os"
 	"os/signal"
@@ -20,16 +21,25 @@ func main() {
 
 	initialize.ViperInit(configPath)
 	initialize.LogInIt()
-
-	err := mqttapp.MqttInit()
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	publish.CreateMqttClient()
+	_, err := initialize.PgInit(configPath)
 	if err != nil {
 		logrus.Fatal(err)
 	}
 
+	err = mqttapp.MqttInit()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	// 创建一个MQTT客户端，并且订阅本地遥测，时间，属性转发消息
+	localMqtt.SubscribeInit()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	// 创建一个MQTT客户端，连接到内网监控站MQTT服务器
+	privateMqtt.CreateMqttClient()
+
+	//	初始化设备注册
 	register.DevRegisterInit()
 
 	gracefulShutdown()
--- gatewayHandler/mqtt/init_config.go
+++ gatewayHandler/mqtt/init_config.go
@@ -8,37 +8,106 @@ import (
 	"github.com/spf13/viper"
 )
 
-var MqttConfig Config
-
-type Config struct {
-	Broker            string    `json:"broker"`
-	User              string    `json:"user"`
-	Pass              string    `json:"pass"`
-	ChannelBufferSize int       `json:"channel_buffer_size"`
-	WriteWorkers      int       `json:"write_workers"`
-	Telemetry         Telemetry `json:"telemetry"`
-}
-type Telemetry struct {
-	SubscribeTopic        string `json:"subscribe_topic"`
-	PublishTopic          string `json:"publish_topic"`
-	GatewaySubscribeTopic string `json:"gateway_subscribe_topic"`
-	GatewayPublishTopic   string `json:"gateway_publish_topic"`
-	QoS                   int    `json:"qos"`
-	PoolSize              int    `json:"pool_size"`
-	BatchSize             int    `json:"batch_size"`
-}
+var PrivateMqttConfig Config
+var LocalMqttConfig Config
 
 func MqttInit() error {
 	// 初始化配置
-	err := loadConfig()
+	err := loadPrivateConfig()
+	if err != nil {
+		return err
+	}
+	err = loadLocalConfig()
 	if err != nil {
 		return err
 	}
 	return nil
 }
-func loadConfig() error {
+
+// 连接到路由器内部MQTT服务器的配置
+func loadLocalConfig() error {
 	var configMap map[string]interface{}
-	// 将 map 映射到 MQTTConfig 结构体
+	// 将 map 映射到 PrivateMqttConfig 结构体
+	// 注意！！！yml文件中带_的key，是无法通过UnmarshalKey解析的
+	err := viper.Unmarshal(&configMap)
+	if err != nil {
+		return fmt.Errorf("unable to decode into struct, %s", err)
+	}
+	// 将 map 转换为 json
+	jsonStr, err := json.Marshal(configMap["mqttLocal"])
+	if err != nil {
+		return fmt.Errorf("unable to marshal config, %s", err)
+	}
+	// 将 json 转换为结构体
+	err = json.Unmarshal(jsonStr, &LocalMqttConfig)
+	if err != nil {
+		return fmt.Errorf("unable to unmarshal config, %s", err)
+	}
+
+	// 打印配置
+	logrus.Debug("local mqtt config:", LocalMqttConfig)
+
+	// 单独获取 broker 配置
+	broker := viper.GetString("mqttLocal.broker")
+	if broker == "" {
+		broker = "localhost:1883"
+		logrus.Println("Using default broker:", broker)
+	}
+	LocalMqttConfig.Broker = broker
+
+	// 单独获取 user 配置
+	user := viper.GetString("mqttLocal.user")
+	if user == "" {
+		user = "root"
+		logrus.Println("Using default user:", user)
+	}
+	LocalMqttConfig.User = user
+
+	// 单独获取 pass 配置
+	pass := viper.GetString("mqttLocal.pass")
+	if pass == "" {
+		pass = "root"
+		logrus.Println("Using default pass:", pass)
+	}
+	LocalMqttConfig.Pass = pass
+
+	// 单独获取 channel_buffer_size 配置
+	channelBufferSize := viper.GetInt("mqttLocal.channel_buffer_size")
+	if channelBufferSize == 0 {
+		channelBufferSize = 10000
+		logrus.Println("Using default channel_buffer_size:", channelBufferSize)
+	}
+	LocalMqttConfig.ChannelBufferSize = channelBufferSize
+
+	// 单独获取 write_workers 配置
+	writeWorkers := viper.GetInt("mqttLocal.write_workers")
+	if writeWorkers == 0 {
+		writeWorkers = 10
+		logrus.Println("Using default write_workers:", writeWorkers)
+	}
+	LocalMqttConfig.WriteWorkers = writeWorkers
+
+	// 单独获取 pool_size 配置
+	poolSize := viper.GetInt("mqttLocal.telemetry.pool_size")
+	if poolSize == 0 {
+		poolSize = 100
+		logrus.Println("Using default pool_size:", poolSize)
+	}
+	LocalMqttConfig.Telemetry.PoolSize = poolSize
+
+	// 单独获取 batch_size 配置
+	batchSize := viper.GetInt("mqttLocal.telemetry.batch_size")
+	if batchSize == 0 {
+		batchSize = 100
+		logrus.Println("Using default batch_size:", batchSize)
+	}
+	return nil
+}
+
+// 连接到内网监控站的配置
+func loadPrivateConfig() error {
+	var configMap map[string]interface{}
+	// 将 map 映射到 PrivateMqttConfig 结构体
 	// 注意！！！yml文件中带_的key，是无法通过UnmarshalKey解析的
 	err := viper.Unmarshal(&configMap)
 	if err != nil {
@@ -50,13 +119,13 @@ func loadConfig() error {
 		return fmt.Errorf("unable to marshal config, %s", err)
 	}
 	// 将 json 转换为结构体
-	err = json.Unmarshal(jsonStr, &MqttConfig)
+	err = json.Unmarshal(jsonStr, &PrivateMqttConfig)
 	if err != nil {
 		return fmt.Errorf("unable to unmarshal config, %s", err)
 	}
 
 	// 打印配置
-	logrus.Debug("mqtt config:", MqttConfig)
+	logrus.Debug("mqtt config:", PrivateMqttConfig)
 
 	// 单独获取 broker 配置
 	broker := viper.GetString("mqtt.broker")
@@ -64,7 +133,7 @@ func loadConfig() error {
 		broker = "localhost:1883"
 		logrus.Println("Using default broker:", broker)
 	}
-	MqttConfig.Broker = broker
+	PrivateMqttConfig.Broker = broker
 
 	// 单独获取 user 配置
 	user := viper.GetString("mqtt.user")
@@ -72,7 +141,7 @@ func loadConfig() error {
 		user = "root"
 		logrus.Println("Using default user:", user)
 	}
-	MqttConfig.User = user
+	PrivateMqttConfig.User = user
 
 	// 单独获取 pass 配置
 	pass := viper.GetString("mqtt.pass")
@@ -80,7 +149,7 @@ func loadConfig() error {
 		pass = "root"
 		logrus.Println("Using default pass:", pass)
 	}
-	MqttConfig.Pass = pass
+	PrivateMqttConfig.Pass = pass
 
 	// 单独获取 channel_buffer_size 配置
 	channelBufferSize := viper.GetInt("mqtt.channel_buffer_size")
@@ -88,7 +157,7 @@ func loadConfig() error {
 		channelBufferSize = 10000
 		logrus.Println("Using default channel_buffer_size:", channelBufferSize)
 	}
-	MqttConfig.ChannelBufferSize = channelBufferSize
+	PrivateMqttConfig.ChannelBufferSize = channelBufferSize
 
 	// 单独获取 write_workers 配置
 	writeWorkers := viper.GetInt("mqtt.write_workers")
@@ -96,7 +165,7 @@ func loadConfig() error {
 		writeWorkers = 10
 		logrus.Println("Using default write_workers:", writeWorkers)
 	}
-	MqttConfig.WriteWorkers = writeWorkers
+	PrivateMqttConfig.WriteWorkers = writeWorkers
 
 	// 单独获取 pool_size 配置
 	poolSize := viper.GetInt("mqtt.telemetry.pool_size")
@@ -104,7 +173,7 @@ func loadConfig() error {
 		poolSize = 100
 		logrus.Println("Using default pool_size:", poolSize)
 	}
-	MqttConfig.Telemetry.PoolSize = poolSize
+	PrivateMqttConfig.Telemetry.PoolSize = poolSize
 
 	// 单独获取 batch_size 配置
 	batchSize := viper.GetInt("mqtt.telemetry.batch_size")
