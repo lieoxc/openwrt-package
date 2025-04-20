b05e9aa4f738e683e8341d87601e446f865fc62a
lieoxc <xiaocongli45@gmail.com>
2025-04-21 00:18:05 +0800

    1. 支持接收命令下发的响应消息 2. 气象监控站增加光照单位 3. 拨号插件增加等待时间（避免重启时拨号失败）

--- iot/files/etc/iot/configs/sql/6.sql
+++ iot/files/etc/iot/configs/sql/6.sql
@@ -1,3 +1,11 @@
 -- 场景管理动作值最大支持到2048
 ALTER TABLE public.scene_action_info 
 ALTER COLUMN action_value TYPE VARCHAR(2048);
+
+-- 增加太阳总辐射的单位
+UPDATE device_model_telemetry
+SET 
+  unit = 'W/m²',
+  updated_at = CURRENT_TIMESTAMP
+WHERE 
+  id = '241de086-8339-5488-91de-d27bf49ec43d';
